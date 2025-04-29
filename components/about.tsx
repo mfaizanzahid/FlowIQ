36524de18528b9d498a9823eb907a0527d659ada
mfaizanzahid <mfaizanzahid@gmail.com>
2025-04-29 14:44:54 +0500

    29-04-25-02

--- components/about.tsx
+++ components/about.tsx
@@ -24,8 +24,8 @@ export default function About() {
             </h2>
 
 
-<div className="grid grid-cols-3 gap-8 md:gap-16 mt-4">
-  <div className="col-span-2 text-left">
+<div className="grid grid-cols-1 gap-8 md:grid-cols-3 md:gap-16 mt-4">
+  <div className="md:col-span-2 text-left">
     <p className="text-lg text-indigo-200/65 mb-4">
       With 20+ years of experience across sales, marketing, operations, project management, and AI development — Faizan knows what it takes to make service businesses tick.
     </p>
@@ -41,21 +41,19 @@ export default function About() {
     </p>
   </div>
   <div>
-    <Spotlight className="group mx-auto grid max-w-sm items-start gap-6 lg:max-w-none lg:grid-cols-1">
-    <a
-      
-      className="group/card relative overflow-hidden rounded-2xl bg-gray-800 p-px before:pointer-events-none before:absolute before:-left-40 before:-top-40 before:z-10 before:h-80 before:w-80 before:translate-x-[var(--mouse-x)] before:translate-y-[var(--mouse-y)] before:rounded-full before:bg-indigo-500/80 before:opacity-0 before:blur-3xl before:transition-opacity before:duration-500 after:pointer-events-none after:absolute after:-left-48 after:-top-48 after:z-30 after:h-64 after:w-64 after:translate-x-[var(--mouse-x)] after:translate-y-[var(--mouse-y)] after:rounded-full after:bg-indigo-500 after:opacity-0 after:blur-3xl after:transition-opacity after:duration-500 hover:after:opacity-20 group-hover:before:opacity-100"
-      href="#0"
-    >
-      <div className="relative z-20 flex h-full items-center justify-center overflow-hidden rounded-[inherit] bg-gray-950 after:absolute after:inset-0 after:bg-linear-to-br after:from-gray-900/50 after:via-gray-800/25 after:to-gray-900/50">
-    <Image
-      className="inline-flex"
-      src={Founder}
-      // width={350}
-      alt="Founder"
-    />
-    </div>
-    </a>
+    <Spotlight className="group mx-auto grid max-w-full items-start gap-6 lg:max-w-none lg:grid-cols-1">
+      <a
+        className="group/card relative overflow-hidden rounded-2xl bg-gray-800 p-px before:pointer-events-none before:absolute before:-left-40 before:-top-40 before:z-10 before:h-80 before:w-80 before:translate-x-[var(--mouse-x)] before:translate-y-[var(--mouse-y)] before:rounded-full before:bg-indigo-500/80 before:opacity-0 before:blur-3xl before:transition-opacity before:duration-500 after:pointer-events-none after:absolute after:-left-48 after:-top-48 after:z-30 after:h-64 after:w-64 after:translate-x-[var(--mouse-x)] after:translate-y-[var(--mouse-y)] after:rounded-full after:bg-indigo-500 after:opacity-0 after:blur-3xl after:transition-opacity after:duration-500 hover:after:opacity-20 group-hover:before:opacity-100"
+        href="#0"
+      >
+        <div className="relative z-20 flex h-full items-center justify-center overflow-hidden rounded-[inherit] bg-gray-950 after:absolute after:inset-0 after:bg-linear-to-br after:from-gray-900/50 after:via-gray-800/25 after:to-gray-900/50">
+          <Image
+            className="inline-flex"
+            src={Founder}
+            alt="Founder"
+          />
+        </div>
+      </a>
     </Spotlight>
   </div>
 </div>
