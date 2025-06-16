903756581f0a0300a2ac16673fe5690ddf87c79a
mfaizanzahid <mfaizanzahid@gmail.com>
2025-06-17 00:07:02 +0500

    inter-semi-bold-font

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
   display: "swap",
-  weight: ["400", "700"],
+  weight: ["400", "500", "600", "700"],
 });
 
 const nacelle = localFont({
